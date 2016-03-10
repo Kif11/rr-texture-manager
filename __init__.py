10ae7d120f36e1f888adf152f95e4230bd6bc5b4
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2016-03-10 11:47:05 -0800

    Added main file

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from rrTextureManager import RRTextureManager
---
+++ rrTextureManager.py
@@ -0,0 +1,27 @@
+import pathlib
+
+class RRTextureManager(object):
+
+    def __init__(self):
+        self.original_paths = []
+        self.temp_paths = []
+
+    @property
+    def count_paths(self, arg):
+        pass
+
+    def add_original_path(self):
+        pass
+
+    def get_temp_paths(self):
+        # idx= orgPath.index(oldPath)
+        # if ( idx>=0):
+        # return changedPath[idx]
+        # else return oldPath
+        pass
+
+    def convert_all_paths(self):
+        # for path in orgPath do:
+        # newP= os.path.basename(path)+ "tmp\" +os.path.dirname(path)
+        # changedPath.append(newP)
+        pass
